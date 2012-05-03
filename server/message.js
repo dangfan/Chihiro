fdee4c6c0dfc200ee74a4853131f51c9afa91634
Kaxitta <kaxitta@kaxitta-Aspire-4745G.(none)>
2012-05-03 13:56:44 +0800

    small changes

--- server/message.js
+++ server/message.js
@@ -15,13 +15,13 @@ function sendMessage(data) {
     socket.get('uid', function (err, uid) {
         var date = new Date();
         redis.hget('users:' + uid, 'nickname', function (err, nickname) {
-            clients[data.uid].emit('messages', 
+            //clients[data.uid].emit('messages', 
                 // from: uid,
                 // nickname: nickname,
                 // time: date,
                 // message: data.msg
                 from: uid
-            });
+            //});
             redis.sadd('oldmessages:' + data.uid, uid + '|' + date + '|' + data.msg);
             console.log('message to:' + data.uid, nickname + '|' + uid + '|' + date + '|' + data.msg);
         });
