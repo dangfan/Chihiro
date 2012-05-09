9927783d236b57fdc9b21bc1248e0ff2b4fb37cb
Kaxitta <kaxitta@kaxitta-Aspire-4745G.(none)>
2012-05-09 15:34:19 +0800

    message little change

--- server/activity.js
+++ server/activity.js
@@ -43,8 +43,8 @@ function addActivity(activity, callback) {
                 redis.set('activity_location:' + activity._id,
                     '[' + activity.mark.longitude + ',' + activity.mark.latitude + ']');
                 redis.sadd('activities_createdby:' + uid, activity._id);
-                redis.sadd('activity_participants:' + activityid, uid);
-                redis.sadd('activities_participate:' + uid, activityid);
+                redis.sadd('activity_participants:' + activity._id, uid);
+                redis.sadd('activities_participate:' + uid, activity._id);
                 callback({err: 0, msg: '添加活动成功'});
             }
         });
