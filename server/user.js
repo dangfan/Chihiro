ccf8c936de91ca8ce1b9a678556ecd68185f14b3
terro <dangf09@gmail.com>
2012-05-09 19:40:02 +0800

    fixed a topic bug

--- server/user.js
+++ server/user.js
@@ -44,6 +44,7 @@ function loadMessages(uid, socket) {
             }
         });
         redisp.on('message', function (channel, msg) {
+            console.log(msg);
             var info = channel.split(':'),
                 data = eval(msg);
             redis.hget('users:' + data.uid, 'nickname', function (err, nickname) {
