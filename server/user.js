5a996846c10acb89650eca1f5e75bfda2b426dba
terro <dangf09@gmail.com>
2012-05-10 11:45:45 +0800

    fixed a friend bug

--- server/user.js
+++ server/user.js
@@ -76,6 +76,7 @@ function login(usr, callback, socket) {
     }
     function next() {
         var tmp = usr.friends;
+        console.log(tmp);
         var length = tmp.length;
         usr.friends = new Array();
         for (var i in tmp) {
